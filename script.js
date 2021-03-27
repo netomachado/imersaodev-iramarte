b12a616abab639f8c1e217e2349bba486b65efe6
Neto Machado <80714222+netomachado@users.noreply.github.com>
2021-03-27 18:31:37 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var seuNome = prompt ('Qual o seu nome?')
+var suaIdade = prompt('Qual a sua idade?')
+var exercicios = prompt('Quantas vezes por semana você pratica exercicio?')
+var poupanca = prompt ('Qual o valor em R$ que você consegue poupar por ano?')
+
+if ( suaIdade < 60 && exercicios > 2 && poupanca > 50000){ alert( seuNome + ' '+'Você tem grandes chances de ir a Marte!')} else{ alert ('É mais provável que voce vá para o Guarujá,kkkk!')}
+alert ('brincadeirinhaaaaa')
